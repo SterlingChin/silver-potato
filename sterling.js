7e60c924379ccf3d361703e4513c8f0781a06aba
Sterling Chin <sterling.chin@gmail.com>
2016-10-10 16:23:22 -0600

    Update sterling.js

--- sterling.js
+++ sterling.js
@@ -2,3 +2,7 @@ var sterling = "Sterling is trying this out for the first time"
 function greetings() {
 return sterling;
 }
+var sterling2 = "sterling's computer is dumb sometimes."
+function greetings2() {
+  return sterling2;
+}
