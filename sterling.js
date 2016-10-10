cb768ae67f5bbd072ed12d7db6ce3c9151eb0415
Sterling <sterling.chin@gmail.com>
2016-10-10 16:37:28 -0600

    Let's try this again

--- sterling.js
+++ sterling.js
@@ -6,3 +6,7 @@ var sterling2 = "sterling's computer is dumb sometimes."
 function greetings2() {
   return sterling2;
 }
+var sterling3 = "let's push this again!"
+function greetings3(){
+  return sterling3;
+}
